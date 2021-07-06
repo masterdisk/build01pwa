d52f02de8463932a233bfd5e3a39ca98961f3350
Eduard Costea <costeaeduardnic@gmail.com>
2021-07-06 12:05:15 +0200

    first commit

---
+++ shopware-pwa.config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  shopwareEndpoint: "http://digitalmarker01build.phct-120.cust.powerhosting.dk/",
+  shopwareAccessToken: "SWSCDULPQGY9BHD_ES21QPR5QA",
+  pwaHost: "https://pwa-demo-api.shopware.com/trunk/",
+};
